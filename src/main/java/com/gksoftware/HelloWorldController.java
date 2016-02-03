6f4cdd736198e822b85383f4802c5e59baffaac1
gksoftware <gordon.kelly@gmail.com>
2016-02-02 17:09:29 -0800

    Cleanup package and imports

--- src/main/java/com/gksoftware/HelloWorldController.java
+++ src/main/java/com/gksoftware/HelloWorldController.java
@@ -1,6 +1,7 @@
+package com.gksoftware;
+
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
